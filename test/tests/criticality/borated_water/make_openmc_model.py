ae0f6997a747df8393d7b35fe10a08dcc0587408
Paul P.H. Wilson <paul.wilson@wisc.edu>
2025-09-30 13:38:16 -0500

    modularize borated water crit test

--- test/tests/criticality/borated_water/make_openmc_model.py
+++ test/tests/criticality/borated_water/make_openmc_model.py
@@ -18,48 +18,51 @@
 
 import openmc
 
-model = openmc.Model()
+def make_uo2_bwater_box(boron_ppm_in=3000):
+  model = openmc.Model()
 
-u = openmc.Material()
-u.set_density('g/cc', 22.0)
-u.add_nuclide('U234', 0.001)
-u.add_nuclide('U235', 0.1)
-u.add_nuclide('U238', 1.0)
-u.add_nuclide('H3', 1e-4)
+  u = openmc.Material()
+  u.set_density('g/cc', 22.0)
+  u.add_nuclide('U234', 0.001)
+  u.add_nuclide('U235', 0.1)
+  u.add_nuclide('U238', 1.0)
+  u.add_nuclide('H3', 1e-4)
 
-bw = openmc.model.borated_water(boron_ppm=3000, density=0.8)
-h1_percent = bw.nuclides[0].percent
-for n in bw.nuclides:
-  print(n.name, n.percent / h1_percent)
+  bw = openmc.model.borated_water(boron_ppm=boron_ppm_in, density=0.8)
+  h1_percent = bw.nuclides[0].percent
+  for n in bw.nuclides:
+    print(n.name, n.percent / h1_percent)
 
-model.materials = openmc.Materials([u, bw])
+  model.materials = openmc.Materials([u, bw])
 
-# Create two cubes side by side
-R = 10.0
-l = openmc.XPlane(x0=0, boundary_type='reflective')
-r = openmc.XPlane(x0=R)
-b = openmc.YPlane(y0=0, boundary_type='reflective')
-t = openmc.YPlane(y0=R, boundary_type='reflective')
-f = openmc.ZPlane(z0=0, boundary_type='reflective')
-k = openmc.ZPlane(z0=R, boundary_type='reflective')
-z = openmc.XPlane(x0=2*R, boundary_type='reflective')
+  # Create two cubes side by side
+  R = 10.0
+  l = openmc.XPlane(x0=0, boundary_type='reflective')
+  r = openmc.XPlane(x0=R)
+  b = openmc.YPlane(y0=0, boundary_type='reflective')
+  t = openmc.YPlane(y0=R, boundary_type='reflective')
+  f = openmc.ZPlane(z0=0, boundary_type='reflective')
+  k = openmc.ZPlane(z0=R, boundary_type='reflective')
+  z = openmc.XPlane(x0=2*R, boundary_type='reflective')
 
-# create a box
-box1 = openmc.Cell(region=+l & -r & +b & -t & +f & -k, fill=u)
-box2 = openmc.Cell(region=+r & -z & +b & -t & +f & -k, fill=bw)
-model.geometry = openmc.Geometry([box1, box2])
+  # create a box
+  box1 = openmc.Cell(region=+l & -r & +b & -t & +f & -k, fill=u)
+  box2 = openmc.Cell(region=+r & -z & +b & -t & +f & -k, fill=bw)
+  model.geometry = openmc.Geometry([box1, box2])
 
-# Finally, define some run settings
-model.settings.batches = 10
-model.settings.inactive = 3
-model.settings.particles = 1000
+  # Finally, define some run settings
+  model.settings.batches = 10
+  model.settings.inactive = 3
+  model.settings.particles = 1000
 
-lower_left = (0, 0, 0)
-upper_right = (R, R, R)
-uniform_dist = openmc.stats.Box(lower_left, upper_right)
-model.settings.source = openmc.source.IndependentSource(space=uniform_dist)
-model.settings.temperature = {'default': 600.0,
-                        'method': 'nearest',
-                        'range': (294.0, 1600.0)}
+  lower_left = (0, 0, 0)
+  upper_right = (R, R, R)
+  uniform_dist = openmc.stats.Box(lower_left, upper_right)
+  model.settings.source = openmc.source.IndependentSource(space=uniform_dist)
+  model.settings.temperature = {'default': 600.0,
+                          'method': 'nearest',
+                          'range': (294.0, 1600.0)}
 
-model.export_to_xml()
+if __name__ == "__main__":
+  model = make_uo2_bwater_box()
+  model.export_to_xml()
