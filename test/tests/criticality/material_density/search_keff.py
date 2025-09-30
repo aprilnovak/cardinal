1de9cc66283d79ca74af53b8c86e0124c8f787df
Paul P.H. Wilson <paul.wilson@wisc.edu>
2025-09-30 13:44:59 -0500

    allow cmd-line to change target

--- test/tests/criticality/material_density/search_keff.py
+++ test/tests/criticality/material_density/search_keff.py
@@ -1,9 +1,15 @@
 import openmc
+import argparse
 
 from make_openmc_model import make_pu_cube
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--target", type=float, default=1.0)
+args = parser.parse_args()
+
 crit_density, guesses, keffs = openmc.search_for_keff(
         make_pu_cube,
+        target=args.target,
         bracketed_method="brentq",
         bracket=[1, 30],
         tol=1e-5,
@@ -13,3 +19,4 @@ for guess, keff in zip(guesses, keffs):
     print( guess, keff)
 
 print("final density ", crit_density)
+
